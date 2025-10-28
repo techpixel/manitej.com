b1a7ad179f1353373e8ad7de25565afa5979112b
techpixel <68567672+techpixel@users.noreply.github.com>
2025-10-27 22:30:42 -0400

    this is warp

--- index.js
+++ index.js
@@ -24,17 +24,18 @@ const svg = document.getElementById('svg-element')
 const owarp = new Warp(svg)
 
 // --- Warp function with "random" smoothness ---
-function warp(x, oy, t, intensity = 1, pinch = 0.5) {
-  const n = oy / 200;        
+function warp(x, oy, t, intensity = 1) {
+  const n = oy / 400;        
+  const mx = x / 20;
 
   let structured =
     (6 * (1 + 0.25 * (n - 0.5))) *
-      Math.sin((2 * Math.PI / 140) * x + (0.3 + 2 * n) + 0.02 * t) +
+      Math.sin((2 * Math.PI / 140) * mx + (0.3 + 2 * n) + 0.02 * t) +
     (3 * (1 - 0.15 * (n - 0.5))) *
-      Math.sin((2 * Math.PI / 63) * x + (1.7 - 1.2 * n) + 0.03 * t);
+      Math.sin((2 * Math.PI / 63) * mx + (1.7 - 1.2 * n) + 0.03 * t);
 
   return {
-	  x: x * 20 + 1.5 * structured,
+	  x: x + 1.5 * structured,
 	  y: oy + intensity * structured
   }
 }
@@ -46,7 +47,7 @@ let offset = 0
 function animate()
 {
 	owarp.transform(([ x, y, ox, oy ]) => {
-		const tf = warp(ox/20, oy, offset, 4);
+		const tf = warp(ox, oy, offset, 4);
 		return [tf.x, tf.y, ox, oy]
 	})
 	offset += 1
