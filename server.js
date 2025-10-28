e26dc59ae1d3d4c7ad56c1b6c74d677fbba4981d
techpixel <68567672+techpixel@users.noreply.github.com>
2025-10-27 23:48:52 -0400

    try ts?

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile('/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.listen(3000, () => {
