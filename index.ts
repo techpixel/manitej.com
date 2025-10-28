dc9e371a26a9bbd51d82abcf6d5a41a065434696
techpixel <68567672+techpixel@users.noreply.github.com>
2025-10-27 23:00:12 -0400

    add express server

---
+++ index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express(); 
+
+app.use(express.static("static"))
+
+app.get("/", (req, res) => {
+	res.sendFile(__dirname + "/index.html");
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
