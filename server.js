1a0d46aea8041a1d98a194a3d00b8c01044522fd
techpixel <68567672+techpixel@users.noreply.github.com>
2025-10-27 23:39:11 -0400

    fix serv

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile('/index.html');
 });
 
 app.listen(3000, () => {
